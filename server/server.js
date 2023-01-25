9b9929b980efd76ccbe0edaf16263daa056fd97d
Blaine Booher <blaine@emergingprairie.com>
2023-01-25 10:53:27 -0600

    adding cors support

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config(); // pull in environment variables
 const express = require('express');
 const { socketServerBuilder } = require('./modules/game.socket');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware setup
 app.use(express.json()); // enables accepting content-type application/json
 app.use(express.static("build/")); // serve out anything you find in the build/ folder (used in production/heroku)
+app.use(cors()); // allow cors
 
 // Socket.io stuff - return an http server with sockets enabled
 // note: this also sets up a router that can access the game state
